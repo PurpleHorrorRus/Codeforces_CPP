16d0dc56c13d6a7571ab9a792ab40a9671e779e1
Maxim <zenit77798@yandex.ru>
2016-04-02 13:27:37 +0800

    cosmetic changes

--- 133A/133A/133A.cpp
+++ 133A/133A/133A.cpp
@@ -21,11 +21,7 @@ int main()
 	string code;
 	cin >> code;
 	if (code.find("H") != -1 || code.find("Q") != -1
-		|| code.find("9") != -1) {
-		cout << "YES";
-	}
-	else {
-		cout << "NO";
-	}
+		|| code.find("9") != -1) cout << "YES";
+	else cout << "NO";
 	return 0;
 }
